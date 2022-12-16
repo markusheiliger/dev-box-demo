e85b3d4c5fd712a9a06a8891fe2117f05fd61c5a
Markus Heiliger <markus.heiliger@dash-two.de>
2022-12-16 17:24:20 +0100

    ...

--- environments/_shared/GetEnvironmentSettings.sh
+++ environments/_shared/GetEnvironmentSettings.sh
@@ -1,4 +1,11 @@
-RESULT=$(az appconfig kv list --subscription $Subscription --name $ConfigurationStore --label $EnvironmentType,\0 --fields key value 2> /dev/null)
-[ -z "$RESULT" ] && RESULT='{}'
-echo $RESULT
-echo $RESULT | jq 'map({ (.key|tostring): .value }) | add' >  $AZ_SCRIPTS_OUTPUT_PATH
+# RESULT=$(az appconfig kv list --subscription $Subscription --name $ConfigurationStore --label $EnvironmentType,\0 --fields key value 2> /dev/null)
+# [ -z "$RESULT" ] && RESULT='{}'
+# echo $RESULT
+# echo $RESULT | jq 'map({ (.key|tostring): .value }) | add' >  $AZ_SCRIPTS_OUTPUT_PATH
+
+outputJson=$(jq -n \
+                --arg foo "FOO" \
+                --arg bar "BAR" \
+                '{foo: $foo, bar: $bar}' )
+
+echo $outputJson > $AZ_SCRIPTS_OUTPUT_PATH
