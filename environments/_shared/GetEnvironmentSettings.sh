873b8a1765820612887bc4b5a625bf0ea110e276
Markus Heiliger <markus.heiliger@dash-two.de>
2022-12-16 18:23:06 +0100

    ...

--- environments/_shared/GetEnvironmentSettings.sh
+++ environments/_shared/GetEnvironmentSettings.sh
@@ -6,7 +6,7 @@
 outputJson=$(jq -n \
                 --arg subscription "$Subscription" \
                 --arg name "$ConfigurationStore" \
-				--arg label "$EnvironmentType,\\0" \
+				--arg label "$EnvironmentType" \
                 '{subscription: $subscription, name: $name, label: $label}' )
 
 echo $outputJson > $AZ_SCRIPTS_OUTPUT_PATH
