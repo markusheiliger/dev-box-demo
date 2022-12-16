dfea9e4c047d05b38aa4cd5bb71f467aef40f808
Markus Heiliger <markus.heiliger@dash-two.de>
2022-12-16 20:26:54 +0100

    ...

--- environments/_shared/GetEnvironmentSettings.sh
+++ environments/_shared/GetEnvironmentSettings.sh
@@ -2,12 +2,12 @@ noLabel=\0
 
 outputJson=$(az appconfig kv list \
 	--only-show-errors \
-	--subscription "$Subscription" \ 
+	--subscription "$Subscription" \
 	--name "$ConfigurationStore" \
 	--label "$EnvironmentType,$noLabel" \
 	--fields key value 2>&1)
 
-if [jq -e . >/dev/null 2>&1 <<<"$json_string"]; then	
+if [jq -e . >/dev/null 2>&1 <<<"$outputJson"]; then	
  	echo $outputJson | jq 'map({ (.key|tostring): .value }) | add' > $AZ_SCRIPTS_OUTPUT_PATH
 else
 	jq -n --arg e "$outputJson" '{error: $e}' > $AZ_SCRIPTS_OUTPUT_PATH
