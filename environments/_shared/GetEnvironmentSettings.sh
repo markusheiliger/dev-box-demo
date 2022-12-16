536a5c741daa0a22266717e8d719a657b960e237
Markus Heiliger <markus.heiliger@dash-two.de>
2022-12-16 20:14:59 +0100

    ...

--- environments/_shared/GetEnvironmentSettings.sh
+++ environments/_shared/GetEnvironmentSettings.sh
@@ -1,15 +1,22 @@
 noLabel=\0
 
-# outputJson=$(az appconfig kv list \
-# 	--subscription "$Subscription" \ 
-# 	--name "$ConfigurationStore" \
-# 	--label "$EnvironmentType,$noLabel" \
-# 	--fields key value | jq 'map({ (.key|tostring): .value }) | add')
+outputJson=$(az appconfig kv list \
+	--only-show-errors \
+	--subscription "$Subscription" \ 
+	--name "$ConfigurationStore" \
+	--label "$EnvironmentType,$noLabel" \
+	--fields key value 2>&1)
 
-outputJson=$(jq -n \
-	--arg s "$Subscription" \
-	--arg n "$ConfigurationStore" \
-	--arg l "$EnvironmentType,$noLabel" \
-	'{subscription: $s, name: $n, label: $l}' )
+if [jq -e . >/dev/null 2>&1 <<<"$json_string"]; then	
+ 	echo $outputJson | jq 'map({ (.key|tostring): .value }) | add' > $AZ_SCRIPTS_OUTPUT_PATH
+else
+	jq -n --arg e "$outputJson" '{error: $e}' > $AZ_SCRIPTS_OUTPUT_PATH
+fi
 
-echo $outputJson > $AZ_SCRIPTS_OUTPUT_PATH
+# outputJson=$(jq -n \
+# 	--arg s "$Subscription" \
+# 	--arg n "$ConfigurationStore" \
+# 	--arg l "$EnvironmentType,$noLabel" \
+# 	'{subscription: $s, name: $n, label: $l}' )
+
+# echo $outputJson > $AZ_SCRIPTS_OUTPUT_PATH
