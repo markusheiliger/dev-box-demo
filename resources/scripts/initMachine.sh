8d094e59850837d3975dbe33d67fd25563e1913b
Markus Heiliger <markus.heiliger@dash-two.de>
2023-01-23 21:03:17 +0100

    ...

--- resources/scripts/initMachine.sh
+++ resources/scripts/initMachine.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export DEBCONF_NOWARNINGS=yes
+export DEBIAN_FRONTEND=noninteractive
+
 # patch needrestart config
 [ -f '/etc/needrestart/needrestart.conf' ] \
 	&& sed -i 's/#$nrconf{restart}.*/$nrconf{restart} = '"'"'l'"'"';/g' /etc/needrestart/needrestart.conf
